0deed8b6feb47b951667ba0be43874e1d5128865
Sreerag Murali <sreeragm46@gmail.com>
2022-08-05 11:38:07 +0530

    fg

--- src/App.js
+++ src/App.js
@@ -115,7 +115,7 @@ class App extends Component {
             <div className="navbar-nav ml-auto">
               <li className="nav-item">
                 <Link to={"/login"} className="nav-link">
-                  Sign In
+                  Sign 
                 </Link>
               </li>
 
