6a04273044787b8deb300e5e07cad9db0757b725
Sreerag Murali <sreeragm46@gmail.com>
2022-08-05 12:53:39 +0530

    test

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://futurerightwings.com/rentsys/api/auth/";
 
 class AuthService {
   login(username, password) {
